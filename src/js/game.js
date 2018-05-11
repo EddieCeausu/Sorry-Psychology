ac5693280c239d27aef0dd085a91be70458a7729
EddieCeausu <eddieswim101@gmail.com>
2018-05-11 16:41:50 -0400

    Js changes

--- src/js/game.js
+++ src/js/game.js
@@ -1,8 +1,3 @@
 if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   window.location.href = 'mobileSite.html';
 }
-
-var questions = { };
-function questionParser() {
-  for
-}
