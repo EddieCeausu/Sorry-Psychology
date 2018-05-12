39f929f05892d66d5ef90bdfd8cf52a910545f5d
EddieCeausu <eddieswim101@gmail.com>
2018-05-12 02:14:38 -0400

    Final changes for the day

--- src/js/game.js
+++ src/js/game.js
@@ -1,29 +1,41 @@
-var questions = ["In________ reinforcement, the reinforcer follows every correct response.\nintermittent\npartial\nnegative\nContinuous", "::D", ]
+var questions = ["In________ reinforcement, the reinforcer follows every correct response.\nintermittent\npartial\nnegative\nContinuous", "::D"]
 
 function questionGetter() {
   var a = questions.pop()
+  console.log(a);
   var q = questions.pop()
-  
-  // TODO:
-  /* Do something here that will seperate the q into different
-  * strings by option and will put the answer at the end */
-  return []
+  var options = []
+  var str = "";
+  for(var i = q.length-1;i >=0;i--) {
+    if(q[i] != "\n"){
+      str = q[i] + str;
+    } else {
+        options.unshift(str);
+        str = "";
+      }
+  }
+  var i = 0
+  for(i = 0; i < q.length; i ++) {
+    if(q[i] == "\n") {
+      break;
+    }
+  }
+return {question : q.slice(0, i - 1), option : options, answer : a.slice(2, 3)}
 }
 function changePreviousQuestion(question, options, answer) {
   var ptag = document.getElementById('Text2');
   ptag.innerHTML = question;
 }
-function changeCurrentQuestion(question) {
-  var q = options[0];
-  options.slice(0,1);
-  var a = options.pop();
+function changeCurrentQuestion(dic) {
 
   var ptag = document.getElementById('Text1');
-  ptag.innerHTML = question;
+  ptag.innerHTML = function() {
+    
+  };
 }
 
 function questionHandler() {
-  var options = questionGetter();
-
+  var quest = questionGetter();
+  changeCurrentQuestion(quest);
 
 }
