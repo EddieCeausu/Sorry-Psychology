4966e812f54682c243c322982ec0065c312739c3
EddieCeausu <eddieswim101@gmail.com>
2018-05-11 23:26:00 -0400

    Graphics and added google analytics

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,28 @@
-if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  window.location.href = 'mobileSite.html';
+var questions = ["In________ reinforcement, the reinforcer follows every correct response.\nintermittent\npartial\nnegative\nContinuous", "::D", ]
+
+function questionGetter() {
+  var a = questions.pop()
+  var q = questions.pop()
+  // TODO:
+  /* Do something here that will seperate the q into different
+  * strings by option and will put the answer at the end */
+  return []
+}
+function changePreviousQuestion(question, options, answer) {
+  var ptag = document.getElementById('Text2');
+  ptag.innerHTML = question;
+}
+function changeCurrentQuestion(question) {
+  var q = options[0];
+  options.slice(0,1);
+  var a = options.pop();
+  
+  var ptag = document.getElementById('Text1');
+  ptag.innerHTML = question;
+}
+
+function questionHandler() {
+  var options = questionGetter();
+
+
 }
