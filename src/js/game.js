fda8f6681e44779f68fc1715725d53e2aa889f9d
EddieCeausu <eddieswim101@gmail.com>
2018-05-12 12:37:42 -0400

    Fixes

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,4 @@
-var questions = ["In________ reinforcement, the reinforcer follows every correct response.\nintermittent\npartial\nnegative\nContinuous", "::D"]
+var questions = ["In________ reinforcement, the reinforcer follows every correct response.\nintermittent\npartial\nnegative\nContinuous", "::D", ]
 
 function questionGetter() {
   var a = questions.pop()
@@ -22,20 +22,36 @@ function questionGetter() {
   }
 return {question : q.slice(0, i - 1), option : options, answer : a.slice(2, 3)}
 }
-function changePreviousQuestion(question, options, answer) {
-  var ptag = document.getElementById('Text2');
-  ptag.innerHTML = question;
-}
-function changeCurrentQuestion(dic) {
 
+function htmCreator(q,o,a) {
+    var input = "<input type=\"radio\" ";
+    var str = "<form action=\"\">" + q + "<br>";
+    for(var i = 0; i < o.length; i++) {
+      str += input + "name=\"Option " + i + "value=\"" + String.fromCharCode(65 + i) + "\"> " + o[i] + "<br>";
+    }
+    return str;
+}
+
+function changePreviousQuestion(dic, response) {
+  var ptag = document.getElementById('Text2');
+  ptag.innerHTML = dic.question;
+  
+}
+
+function changeCurrentQuestion(dic) {
+  var q = dic.question;
+  var o = dic.option;
+  var a = dic.answer;
   var ptag = document.getElementById('Text1');
-  ptag.innerHTML = function() {
-    
-  };
+  ptag.innerHTML = htmCreator(q,o,a);
+}
+
+function qCheck(response,answer) {
+  if(resposnse == answer) return true;
+  return false;
 }
 
 function questionHandler() {
   var quest = questionGetter();
   changeCurrentQuestion(quest);
-
 }
