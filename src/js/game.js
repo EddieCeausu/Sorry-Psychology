a69d1c367520920e26d3d7b7f3e6408778b999eb
EddieCeausu <eddieswim101@gmail.com>
2018-05-12 00:55:15 -0400

    Changes

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,7 @@ var questions = ["In________ reinforcement, the reinforcer follows every correct
 function questionGetter() {
   var a = questions.pop()
   var q = questions.pop()
+  
   // TODO:
   /* Do something here that will seperate the q into different
   * strings by option and will put the answer at the end */
@@ -16,7 +17,7 @@ function changeCurrentQuestion(question) {
   var q = options[0];
   options.slice(0,1);
   var a = options.pop();
-  
+
   var ptag = document.getElementById('Text1');
   ptag.innerHTML = question;
 }
